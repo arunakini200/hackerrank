60458570a5ae8316b5b1553a6bbd38a8aae76150
Aruna.M <arunakini2004@gmail.com>
2026-03-07 15:55:17 +0530

    Create miniongame.py

---
+++ python/miniongame.py
@@ -0,0 +1,19 @@
+def minion_game(string):
+    vowels = "AEIOU"
+    kevin_score = 0
+    stuart_score = 0
+    n = len(string)
+    
+    for i in range(n):
+        if string[i] in vowels:
+            kevin_score += (n - i)
+        else:
+            stuart_score += (n - i)
+            
+    if stuart_score > kevin_score:
+        print(f"Stuart {stuart_score}")
+    elif kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    else:
+        print("Draw")
+
